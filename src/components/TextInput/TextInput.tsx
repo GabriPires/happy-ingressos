1f67c776ef7b8727bb81d767c5519b6a6ce1e767
Gabriel Pires <gabpiresdev@gmail.com>
2022-11-07 21:12:51 -0300

    refactor: text input colors changed

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -2,11 +2,13 @@ import { styled } from '../../styles';
 
 export const TextInput = styled('input', {
   border: 'none',
+  background: '$gray800',
   borderRadius: '4px',
-  height: '35px',
   width: '100%',
   paddingInline: '10px',
+  paddingTop: '12px',
+  paddingBottom: '12px',
 
   fontSize: '$md',
-  color: '$gray900',
+  color: '$gray300',
 });
